53deec1086eee6c236e7e05471aff8ce0997c9f0
Sven64-45 <149205320+Sven64-45@users.noreply.github.com>
2023-11-23 07:35:14 +0100

    Add files via upload
    ajout fonction ajout photo

--- dyn.js
+++ dyn.js
@@ -102,6 +102,7 @@ document.getElementById('js-logout').addEventListener('click', logout);
 // ***************** affichage modale *****************
 
 let modal =null
+let dataAdmin;
 
 // purge gallery modale
 
@@ -117,6 +118,11 @@ const openModale = function(event) {
     modal.style.display = null
     modal.setAttribute("aria-hidden", "false")
     modal.setAttribute("aria-modal", "true")
+
+    document.querySelectorAll(".js-modale-projet").forEach (a => {
+        a.addEventListener("click", openModaleProjets);
+    });
+
     modal.addEventListener("click", closeModale) 
     modal.querySelector(".js-modale-close").addEventListener("click", closeModale)
     modal.querySelector(".js-modale-stop").addEventListener("click", stopPropagation)
@@ -151,24 +157,24 @@ window.addEventListener("keydown", function(event) {
 
 async function galleryModale() {
     const response = await fetch(api_url); 
-    data = await response.json();
+    dataAdmin = await response.json();
     const galleryModale = document.querySelector(".js-admin-gallery");
     purgeGalleryModale();
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < dataAdmin.length; i++) {
 
         const div = document.createElement("div");
         div.classList.add("gallery-modale-projet");
         galleryModale.appendChild(div);
 
         const img = document.createElement("img");
-        img.src = data[i].imageUrl;
-        img.alt = data[i].title;
+        img.src = dataAdmin[i].imageUrl;
+        img.alt = dataAdmin[i].title;
         div.appendChild(img);
 
         const p = document.createElement("p");
         div.appendChild(p);
-        p.classList.add(data[i].id, "js-modale-projet-suppr");
+        p.classList.add(dataAdmin[i].id, "js-modale-projet-suppr");
 
         const icon = document.createElement("i");
         icon.classList.add("fa-solid", "fa-trash-can");
@@ -190,9 +196,9 @@ function deleteProjet () {
 };
 
 async function deleteImage() {
-    console.log("DEBUG SUPRESSION")
-    console.log(this.classList[0])
-    console.log(token)
+    // console.log("DEBUG SUPRESSION")
+    // console.log(this.classList[0])
+    // console.log(token)
     await fetch(`http://localhost:5678/api/works/${this.classList[0]}`, {
         method: "DELETE",
         headers: { Authorization: `Bearer ${token}`},
@@ -245,8 +251,9 @@ const openModaleProjets = function(event) {
 };
 
 const closeModaleProjets = function(event) {   
-    if (modaleProjets === null) return
-    event.preventDefault();
+    if (modaleProjets === null) 
+    return
+    // event.preventDefault();
 
     modaleProjets.setAttribute("aria-hidden", "true")
     modaleProjets.removeAttribute("aria-modal")
@@ -266,10 +273,101 @@ const retourModale = function(event) {
     event.preventDefault();
     modaleProjets.style.display = "none"
     modaleProjets = null
-    // modaleProjets(data)
+    modaleProjets(dataAdmin)
 };
 
 // listner sur le bouton ajouter projet
-document.querySelectorAll(".js-modale-projet").forEach (a => {
-    a.addEventListener("click", openModaleProjets);
-});
+// document.querySelectorAll(".js-modale-projet").forEach (a => {
+//     a.addEventListener("click", openModaleProjets);
+// });
+
+// listener sur le bouton validation ajouter projet
+const btnAjouterProjet = document.querySelector(".js-add-work");
+btnAjouterProjet.addEventListener("click", addProjet);
+
+// ************ AJOUT PROJET ************
+
+// async function addProjet(event) {
+//     event.preventDefault();
+//     // console.log("DEBUG AJOUT PROJET")
+//     // console.log(token)
+//     const form = document.querySelector(".modale-projet-form");
+//     const formData = new FormData(form);
+//     const dataForm = Object.fromEntries(formData);
+//     console.log(dataForm)
+//     await fetch("http://localhost:5678/api/works", {
+//         method: "POST",
+//         headers: { Authorization: `Bearer ${token}`},
+//         body: JSON.stringify(dataForm),
+//     })
+//     // console.log(JSON.stringify(dataForm));
+//     .then (response => {
+//         console.log(response)
+//         // Token good
+//         if (response.status === 201) {
+//             console.log("DEBUG AJOUT DU PROJET")
+//             // refreshPage(dataForm)
+//             alert("Projet ajouté avec succès :)");
+//             modaleProjets(dataAdmin);
+//             backToModale(event);
+//             generationProjets(data, null);
+//         }
+//         // Token inorrect
+//         else if (response.status === 401) {
+//             alert("Vous n'êtes pas autorisé à ajouter un projet, merci de vous connecter avec un compte valide")
+//             window.location.href = "login.html";
+//         }
+//     })
+//     .catch (error => {
+//         console.log(error)
+//     })
+// }
+
+async function addProjet(event) {
+
+    event.preventDefault();
+
+    const title = document.querySelector(".js-title").value;
+    const categoryId = document.querySelector(".js-categoryId").value;
+    const image = document.querySelector(".js-image").files[0];
+
+    if (title === "" || categoryId === "" || image === undefined) {
+        alert("Merci de remplir tous les champs");
+        return;
+    } else if (categoryId !== "1" && categoryId !== "2" && categoryId !== "3") {
+        alert("Merci de choisir une catégorie valide");
+        return;
+        } else {
+            try {
+                const formData = new FormData();
+                formData.append("title", title);
+                formData.append("category", categoryId);
+                formData.append("image", image);
+
+                const response = await fetch("http://localhost:5678/api/works", {
+                    method: "POST",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    body: formData,
+                });
+                console.log(response);
+                console.log(formData);
+                if (response.status === 201) {
+                    alert("Projet ajouté avec succès :)");
+                    galleryModale(dataAdmin);
+                    retourModale(event);
+                    getProjets(data, null);
+                    
+                } else if (response.status === 400) {
+                    alert("Merci de remplir tous les champs");
+                } else if (response.status === 500) {
+                    alert("Erreur serveur");
+                } else if (response.status === 401) {
+                    alert("Vous n'êtes pas autorisé à ajouter un projet");
+                    window.location.href = "login.html";
+            }}
+
+            catch (error) {
+                console.log(error);
+}}}
